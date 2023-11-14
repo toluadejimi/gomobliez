949a1a6d8cb4c0d697453bbe10308566f35a295d
Jimmy <toluadejimi@gmail.com>
2023-11-14 16:18:17 +0100

    uusuusu

--- routes/api.php
+++ routes/api.php
@@ -152,7 +152,7 @@ Route::group(['middleware' => ['auth:api', 'acess']], function () {
     Route::post('send-message', [NumberController::class, 'send_message']);
     Route::get('get-user', [ProfileController::class, 'get_user']);
     Route::get('get-messages', [NumberController::class, 'get_message']);
-    Route::get('open-message', [NumberController::class, 'open_message']);
+    Route::post('open-message', [NumberController::class, 'open_message']);
     Route::get('get-a-message', [NumberController::class, 'get_a_message']);
     Route::post('initiate-call', [NumberController::class, 'start_call']);
 
