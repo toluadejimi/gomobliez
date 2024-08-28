41ef9907ca4549b42179141c7d8a861d968c40e2
Jimmy <toluadejimi@gmail.com>
2024-08-28 13:17:11 +0100

    user id added

--- routes/api.php
+++ routes/api.php
@@ -91,7 +91,7 @@ Route::group(['middleware' => ['auth:api', 'acess']], function () {
     Route::any('conversion-rate', [PaymentController::class, 'conversion_rate']);
     Route::post('send-to-bank', [PaymentController::class, 'send_to_bank']);
 
-    Route::post('create-transfer-pin', [ProfileController::class, 'create_transfer_pin']);
+    Route::post('create-pin', [ProfileController::class, 'create_transfer_pin']);
     Route::post('subscribe', [ProfileController::class, 'subscribe_plan']);
     Route::post('reset-pin', [ProfileController::class, 'reset_pin']);
    // Route::post('reset-password', [ProfileController::class, 'reset_password']);
